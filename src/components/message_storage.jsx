cc25c7a44108f3a4823c80a1a189b21a84bae36d
tiggerDen <honeydetective786@gmail.com>
2024-02-20 15:58:01 +0300

    clean

--- src/components/message_storage.jsx
+++
@@ -1,2 +0,0 @@
-//Implement local message storage that checks user_id to load messages for that user from local storage. Every sent/recieved message should be stored in local storage and assigned to a chat consisting of 2 users.
-
